9807a93f9c0fe9581b8d6fc451658786a2507cf1
Shusui MOYATANI <syusui.s@gmail.com>
2025-02-16 21:52:36 +0900

    refactor: split verifyIntegrity

--- src/gmFetch.test.ts
+++ src/gmFetch.test.ts
@@ -1,10 +1,7 @@
-import { webcrypto } from 'node:crypto';
-
 import assert from 'assert';
 import {
   parseXHRHeaders,
   buildResponse,
-  verifyIntegrityWith,
 } from '@/gmFetch';
 
 test('parseXHRHeader should parse standard headers', () => {
@@ -109,54 +106,4 @@ test('buildResponse should return expected Response', async () => {
   assert(actual.headers.get('Content-Type') === 'text/html; charset=UTF-8');
   assert(actual.headers.get('Server') === 'Dummy Server 1.2.3');
   assert(actual.headers.get('X-Frame-Options') === 'SAMEORIGIN');
-});
-
-test('verifyIntegrity should deny malformed integrity', async () => {
-  const verifyIntegrity = verifyIntegrityWith(webcrypto as Crypto);
-
-  await expect(async () => verifyIntegrity('', new Blob([]))).rejects.toThrow();
-  await expect(async () => verifyIntegrity('sha256-', new Blob([]))).rejects.toThrow();
-  await expect(async () => verifyIntegrity('sha384-', new Blob([]))).rejects.toThrow();
-  await expect(async () => verifyIntegrity('sha512-', new Blob([]))).rejects.toThrow();
-});
-
-test('verifyIntegrity should allow valid integrity', async () => {
-  const verifyIntegrity = verifyIntegrityWith(webcrypto as Crypto);
-
-  const input = new Uint8Array(new ArrayBuffer(256));
-  for (let i = 0; i < input.length; i += 1) input[i] = i;
-
-  const sha256Integrity = 'sha256-QK/y6dLYki5Hr9RkjmlnSXFYeF+9Hahw5xECZr+USIA=';
-  await verifyIntegrity(sha256Integrity, new Blob([input]));
-
-  const sha384Integrity = 'sha384-/9rr/2XtBc9ADwIhxMz7SyEE+2pR+H5AvmxDCThr/ewokukXmzRjIzGllZJzfbXF';
-  await verifyIntegrity(sha384Integrity, new Blob([input]));
-
-  const sha512Integrity =
-    'sha512-HnuAvI7cVSyP7rJ4DhEUd+W8cEZfrBp3sps1mAw/DOSgNqbJRiA2gkvVaAHmKvfp/rpcIu2KWvh3v33hF9ysbQ==';
-  await verifyIntegrity(sha512Integrity, new Blob([input]));
-});
-
-test('verifyIntegrity should deny invalid integrity', async () => {
-  const verifyIntegrity = verifyIntegrityWith(webcrypto as Crypto);
-
-  const input = new Uint8Array(new ArrayBuffer(256));
-  for (let i = 0; i < input.length; i += 1) input[i] = i;
-
-  await expect(async () => {
-    const sha256Integrity = 'sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-    await verifyIntegrity(sha256Integrity, new Blob([input]));
-  }).rejects.toThrow();
-
-  await expect(async () => {
-    const sha384Integrity =
-      'sha384-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-    await verifyIntegrity(sha384Integrity, new Blob([input]));
-  }).rejects.toThrow();
-
-  await expect(async () => {
-    const sha512Integrity =
-      'sha512-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-    await verifyIntegrity(sha512Integrity, new Blob([input]));
-  }).rejects.toThrow();
 });
--- src/gmFetch.ts
+++ src/gmFetch.ts
@@ -1,3 +1,4 @@
+import { verifyIntegrity } from '@/utils/verifyIntegrity';
 import arrayBufferToString from '@/utils/arrayBufferToString';
 
 const HTTPMethods = [
@@ -153,40 +154,6 @@ export const parseXHRHeaders = (gmHeaders: string | null): Headers => {
 
   return headers;
 };
-
-const algHash: Record<string, string> = {
-  sha256: 'SHA-256',
-  sha384: 'SHA-384',
-  sha512: 'SHA-512',
-};
-
-/**
- * https://w3c.github.io/webappsec-subresource-integrity/#the-integrity-attribute
- */
-export const verifyIntegrityWith =
-  (crypto: Crypto) =>
-  async (integrity: string, data: Blob): Promise<void> => {
-    if (crypto?.subtle?.digest == null) {
-      throw new Error('digest function is not available.');
-    }
-
-    const match = integrity.match(/^(sha(?:256|384|512))-([a-zA-Z0-9+/=]+)$/);
-
-    if (!match) {
-      throw new TypeError('unsupported alg or integrity format');
-    }
-
-    const [, alg, expected] = match;
-
-    const hashAlgorithm = algHash[alg] || alg;
-    const digest = await globalThis.crypto.subtle.digest(hashAlgorithm, await data.arrayBuffer());
-    const actual = btoa(encodeArrayBufferAsIsomorphicEncodedString(digest));
-    if (actual !== expected) {
-      throw new Error('Integrity verification failed');
-    }
-  };
-
-export const verifyIntegrity = verifyIntegrityWith(globalThis.crypto);
 
 /**
  * Convert GM.Response to Response
