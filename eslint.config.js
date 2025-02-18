07db14d858582b11e5cfe22d41ff2b29299f545a
Shusui MOYATANI <syusui.s@gmail.com>
2025-02-19 01:01:38 +0900

    chore: update eslint

---
+++ eslint.config.js
@@ -0,0 +1,100 @@
+import eslint from '@eslint/js';
+import configPrettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import globals from 'globals';
+import typescriptEslint, {
+  configs as typescriptEslintConfigs,
+  parser as typescriptEslintParser,
+} from 'typescript-eslint';
+
+export default typescriptEslint.config(
+  {
+    ignores: ['dist/', '.rollup.cache/'],
+  },
+  eslint.configs.recommended,
+  typescriptEslintConfigs.recommendedTypeChecked,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  configPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        parser: typescriptEslintParser,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: [
+      '**/*.{ts,js,mjs}',
+      'eslint.config.js',
+      'rollup.config.js',
+    ],
+    extends: [
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      pluginImport.flatConfigs.recommended,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      pluginImport.flatConfigs.typescript,
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          extensions: ['.mjs', '.js', '.ts', '.d.ts'],
+        },
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          fixStyle: 'inline-type-imports',
+          prefer: 'type-imports',
+        },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      'import/no-extraneous-dependencies': 'off',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    name: 'test',
+    files: ['**/*.test.ts'],
+    rules: {
+      'func-names': 'off',
+    },
+  },
+);
